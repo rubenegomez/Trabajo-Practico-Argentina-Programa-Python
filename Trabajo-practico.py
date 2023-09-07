5b82b19ca668c21463918f32499b383f6f371f42
rubengomez <rubeng69@hotmail.com>
2023-09-07 10:48:30 -0300

    commit 3

--- Trabajo-practico.py
+++ Trabajo-practico.py
@@ -3,6 +3,8 @@ import numpy as np
 import datetime as dt
 pd.DataFrame()
 
-df = pd.read_csv("D:\RUBEN\Downloads\Spotify 2010 - 2021 Top 100.csv")
+df = pd.read_csv("D:\RUBEN\Downloads\Spotify 2010 - 2021 Top 100.csv", sep=",")
 df.info()
 print(df)
+indie = df[df["top genre"]== "indie rock"]["dur"].sum()
+print(indie)
