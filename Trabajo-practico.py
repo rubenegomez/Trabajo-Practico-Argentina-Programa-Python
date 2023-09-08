add303dffce97ca8415465aabf1d40204c52e6b7
rubengomez <rubeng69@hotmail.com>
2023-09-08 10:12:38 -0300

    comit 08/09/23 10 :12

--- Trabajo-practico.py
+++ Trabajo-practico.py
@@ -3,8 +3,21 @@ import numpy as np
 import datetime as dt
 pd.DataFrame()
 
-df = pd.read_csv("D:\RUBEN\Downloads\Spotify 2010 - 2021 Top 100.csv", sep=",")
-df.info()
-print(df)
-indie = df[df["top genre"]== "indie rock"]["dur"].sum()
-print(indie)
+data = pd.read_csv("D:\RUBEN\Downloads\Spotify 2010 - 2021 Top 100.csv", sep=",")
+
+#indie_dur = df[df["top genre"]== "indie rock"]["dur"].sum()
+
+#print(indie_dur)
+class PlayList():
+    def __init__(self,filtro, filtro1):
+        self.filtro = filtro
+        self.filtro1 = filtro1
+        
+    def generador_de_listas(filtro, filtro1):
+        lista = data[data[filtro] == filtro1]
+        return lista
+    
+indie = PlayList.generador_de_listas("top genre", "indie rock")
+    
+    
+print(indie)    
