8f3caf910384223d098423b428407189f4a27e75
rubengomez <rubeng69@hotmail.com>
2023-09-09 13:54:02 -0300

    comit 9/9/23 13:53

--- Trabajo-practico.py
+++ Trabajo-practico.py
@@ -3,15 +3,19 @@ import numpy as np
 import datetime as dt
 pd.DataFrame()
 
-data = pd.read_csv("D:\RUBEN\Downloads\Spotify 2010 - 2021 Top 100.csv", sep=",")
+data = pd.read_csv("Spotify 2010 - 2021 Top 100.csv", sep=",")
 
-#indie_dur = df[df["top genre"]== "indie rock"]["dur"].sum()
-
-#print(indie_dur)
 class PlayList():
-    def __init__(self,filtro, filtro1):
+    def __init__(self,filtro, filtro1, filtro2, orden):
         self.filtro = filtro
         self.filtro1 = filtro1
+        self.filtro2 = filtro2
+        self.orden = orden
+        
+    def lista_generica():
+        lista = data
+        return lista
+        
         
     def generador_de_listas(filtro, filtro1):
         lista = data[data[filtro] == filtro1]
@@ -23,14 +27,36 @@ class PlayList():
         sobrante1 =duracion%3600
         minutos = sobrante1//60
         sobrante2 = sobrante1%60
-        duracion_total = horas, minutos, sobrante2
+        duracion_total = "Horas", horas,"Minutos", minutos,"segundos", sobrante2
         return duracion_total
     
+    def ordenar_lista(filtro, filtro1, filtro2, orden):
+        ordenar = True
+        if orden == "asendente":
+            ordenar = False
+        lista_ordenada = data[data[filtro]== filtro1].sort_values(by= filtro2, ascending=ordenar)
+        return lista_ordenada
+    
+    def list_cant_item(filtro, filtro1):
+        cantidad = data[data[filtro]== filtro1].count(axis=0)
+        return cantidad
     
 indie = PlayList.generador_de_listas("top genre", "indie rock")
 indie_duracion = PlayList.calcula_duracion("top genre", "indie rock")
-    
-    
-print(indie)   
+indie_ordenada = PlayList.ordenar_lista("top genre", "indie rock", "title", "desendente")
+indie_ordenada1 = PlayList.ordenar_lista("top genre", "indie rock", "artist", "asendente")
+indie_cantidad_temas = PlayList.list_cant_item("top genre", "indie rock")
+lista_generica = PlayList.lista_generica()
 
-print(indie_duracion) 
+    
+#print(indie)   
+
+#print(f"La lista dura {indie_duracion}")
+
+#print(indie_ordenada)
+
+#print(indie_ordenada1)
+
+#print(indie_cantidad_temas)
+
+#print(lista_generica)
