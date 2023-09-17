590e44b02e800abb08607d28ca815e0cf5481bca
rubengomez <rubeng69@hotmail.com>
2023-09-17 20:03:53 -0300

    17/9/23 20:03

--- graficos.py
+++ graficos.py
@@ -8,16 +8,10 @@ nombre_archivo = "Spotify 2010 - 2021 Top 100.csv"
 def leer_archivo(nombre_archivo):
     data = pd.read_csv(nombre_archivo, sep=",")
     return data
-
 data = leer_archivo(nombre_archivo)
-
-
-
 ejex = list(data["year released"].unique())
-ejey = list(data[data["top genre"] == "dance pop"].value_counts())
-    
-
+ejey = []
+for genero in ejex:
+    ejey.append(len(data[data["year released"]== genero]))
 fig, ax = plt.subplots()   
 ax.bar(ejex, ejey)    
-
-
