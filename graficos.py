a139cdbf337bcf0f62473ae38c522da997ebb351
rubengomez <rubeng69@hotmail.com>
2023-09-19 11:40:55 -0300

    comit 19/9/23 11:40

--- graficos.py
+++ graficos.py
@@ -15,3 +15,32 @@ for genero in ejex:
     ejey.append(len(data[data["year released"]== genero]))
 fig, ax = plt.subplots()   
 ax.bar(ejex, ejey)    
+
+
+
+
+def calcula_duracion(filtro, filtro1):
+        duracion = data[data[filtro] == filtro1]["dur"].sum()  
+        horas = duracion//3600
+        sobrante1 =duracion%3600
+        minutos = sobrante1//60
+        sobrante2 = sobrante1%60
+        horas = horas 
+        minutos = minutos 
+        segundos = sobrante2
+        return horas, minutos, segundos
+    
+subtotal = calcula_duracion("top genre", "dance pop")    
+    
+    
+def calcula_duracion_2():
+        hora1 = subtotal // 360
+        sobra = hora1%360
+        minutos1 = sobra // 60
+        sobra1 = sobra%60
+        
+        return hora1, minutos1, sobra1
+    
+    
+print(calcula_duracion_2)         
+        
